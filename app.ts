374fb4849d69add561398514cc4fe1df3e01f7ac
Mo <mmodoucham@outlook.com>
2023-08-30 17:00:18 +0300

    Implement Swagger Documentation for API Endpoints

--- app.ts
+++ app.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express";
 
 import { createTasksTable } from "./db";
 import tasksRouter from "./src/routes/index";
+import swaggerSetup from "./src/utils/swagger";
 
 const app = express();
 const PORT: number = parseInt(process.env.PORT as string, 10) || 3000;
@@ -13,13 +14,32 @@ app.use(express.json());
 createTasksTable();
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send({
+    message:
+      "Oh, congratulations!  I'm sure you were expecting a grand treasure trove of data, but alas, you're in the wrong place. How about you make your way to /docs instead? There, you might actually find something useful. Maybe. 🙃",
+  });
+});
+
+/**
+ * @swagger
+ * /healthcheck:
+ *  get:
+ *     tags:
+ *     - Healthcheck
+ *     description: Responds if the API is up and running
+ *     responses:
+ *       200:
+ *         description: API is up and running
+ */
+app.get("/healthcheck", (req: Request, res: Response) => {
+  res.status(200).send("API is up and running");
 });
 
 app.use("/api/tasks", tasksRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  swaggerSetup(app, PORT);
 });
 
 export default app;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,3 +1,55 @@
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Tasks:
+ *       type: object
+ *       required:
+ *         - title
+ *         - description
+ *         - duedate
+ *       properties:
+ *         id:
+ *           type: string
+ *           description: The auto-generated id of the task
+ *         title:
+ *           type: string
+ *           description: The title of the task
+ *         description:
+ *           type: string
+ *           description: A brief description of the task
+ *         duedate:
+ *           type: string
+ *           format: date
+ *           description: The due date of the task
+ *       example:
+ *         id: 1
+ *         title: A Sample Task
+ *         description: Sample Description.
+ *         duedate: 2024-08-24T14:15:22Z
+ *     CreateTask:
+ *       type: object
+ *       required:
+ *         - title
+ *         - description
+ *         - duedate
+ *       properties:
+ *         title:
+ *           type: string
+ *           description: The title of the task
+ *         description:
+ *           type: string
+ *           description: A brief description of the task
+ *         duedate:
+ *           type: string
+ *           format: date
+ *           description: The due date of the task
+ *       example:
+ *         title: A Sample Task
+ *         description: Write a detailed proposal for the upcoming project.
+ *         duedate: 2024-08-24T14:15:22Z
+ */
+
 import { Router } from "express";
 import {
   getTasks,
@@ -9,10 +61,136 @@ import {
 
 const router = Router();
 
+/**
+ * @swagger
+ * /api/tasks:
+ *   get:
+ *     summary: Lists all the tasks
+ *     tags: [Tasks]
+ *     responses:
+ *       "200":
+ *         description: The list of tasks.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Tasks'
+ *       "500":
+ *         description: Internal server error.
+ */
+
 router.get("/", getTasks);
+
+/**
+ * @swagger
+ * /api/tasks/{id}:
+ *   get:
+ *     summary: Gets a task by ID
+ *     tags: [Tasks]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The task ID
+ *     responses:
+ *       "200":
+ *         description: The requested task.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Tasks'
+ *       "404":
+ *         description: Task not found.
+ *       "500":
+ *         description: Internal server error.
+ */
+
 router.get("/:id", getTaskById);
+/**
+ * @swagger
+ * /api/tasks/:
+ *   post:
+ *     summary: Creates a new task
+ *     tags:
+ *       - Tasks
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateTask'
+ *     responses:
+ *       "201":
+ *         description: The created task.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Tasks'
+ *       "400":
+ *         description: Invalid request body.
+ *       "500":
+ *         description: Internal server error.
+ */
+
 router.post("/", createTask);
+
+/**
+ * @swagger
+ * /api/tasks/{id}:
+ *   put:
+ *     summary: Updates a task
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The task id
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/CreateTask'
+ *     responses:
+ *       "200":
+ *         description: Update was successful.
+ *       "400":
+ *         description: Invalid request body.
+ *       "404":
+ *         description: Task not found.
+ *       "500":
+ *         description: Internal server error.
+ */
+
 router.put("/:id", updateTask);
+
+/**
+ * @swagger
+ * /api/tasks/{id}:
+ *   delete:
+ *     summary: Deletes a task by id
+ *     tags:
+ *       - Tasks
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: The task id
+ *     responses:
+ *       "204":
+ *         description: Delete was successful.
+ *       "404":
+ *         description: Task not found.
+ *       "500":
+ *         description: Internal server error.
+ */
+
 router.delete("/:id", deleteTask);
 
 export default router;
