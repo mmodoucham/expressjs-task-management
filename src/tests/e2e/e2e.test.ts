9d65fa1fc4e77289d415f0d8fc733c91acbc250b
Mo <mmodoucham@outlook.com>
2023-08-30 23:13:51 +0300

    File rename e2e

--- src/tests/e2e/e2e.test.ts
+++ src/tests/e2e/e2e.test.ts
@@ -13,20 +13,24 @@ test("Request for all tasks", async () => {
   const response = await request(app).get("/api/tasks");
   expect(response.statusCode).toBe(200);
 });
+
 test("Request for a single task", async () => {
   const response = await request(app).get(`/api/tasks/${testTaskID}`);
   expect(response.statusCode).toBe(200);
 });
+
 test("Request for a non-existent task", async () => {
   const response = await request(app).get("/api/tasks/100");
   expect(response.statusCode).toBe(404);
 });
+
 test("Create a task", async () => {
   const response = await request(app).post("/api/tasks").send(testTask);
   expect(response.statusCode).toBe(201);
   expect(response.body.title).toEqual(testTask.title);
   expect(response.body.description).toEqual(testTask.description);
 });
+
 test("Update a task", async () => {
   const response = await request(app)
     .put(`/api/tasks/${testTaskID}`)
@@ -35,14 +39,17 @@ test("Update a task", async () => {
   expect(response.body.title).toEqual(testTask.title);
   expect(response.body.description).toEqual(testTask.description);
 });
+
 test("Delete a task", async () => {
   const response = await request(app).delete(`/api/tasks/${testTaskID}`);
   expect(response.statusCode).toBe(204);
 });
+
 test("Update a non-existent task", async () => {
   const response = await request(app).put(`/api/tasks/100`).send(testTask);
   expect(response.statusCode).toBe(404);
 });
+
 test("Delete a non-existent task", async () => {
   const response = await request(app).delete(`/api/tasks/100`);
   expect(response.statusCode).toBe(404);
